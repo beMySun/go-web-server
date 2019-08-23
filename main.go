f99a546ec99045f586f95e686898cd924c95f6f0
KK <yongkang.yan@shopee.com>
2019-08-23 14:30:29 +0800

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "{name: 'kk'}",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
